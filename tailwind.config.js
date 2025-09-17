20b1aa1fbb3f76cf224746388d1ebd07e25e6aff
prerakbisen <prerakbisen@gmail.com>
2025-09-17 22:37:10 +0530

    updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -130,4 +130,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
